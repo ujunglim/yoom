e7444c24da3a05817e7689cda8d76c5970eb5fb3
Yuyu <ujunglim@naver.com>
2021-08-15 11:01:23 +0800

    frontend setup

--- src/server.js
+++ src/server.js
@@ -2,6 +2,19 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+// set view engine by pug
+app.set("view engine", "pug");
+// tell express where are templates
+app.set("views", __dirname + "/views");
 
-app.listen(3000);
+// do static
+// create public url to share some files to users
+// when user goes to /public, show __dirname + "/public" folder
+app.use("/public", express.static(__dirname + "/public"));
+
+// render a view
+// create route hanlder which renders home.pug
+app.get("/", (req, res) => res.render("home"));
+
+const handleListen = () => console.log("Listening on http://localhost:3000")
+app.listen(3000, handleListen);

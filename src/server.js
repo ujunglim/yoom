3d28107c7e4de8b0b3b9c6330f72be1d33e42707
Yuyu <ujunglim@naver.com>
2021-09-12 16:47:11 +0800

    websocket events

--- src/server.js
+++ src/server.js
@@ -27,4 +27,10 @@ const server = http.createServer(app);
 // create WebSocket server
 const wss = new WebSocket.Server({ server });
 
+function handleConnection(socket) {
+  console.log(socket);
+}
+
+wss.on("connection", handleConnection);
+
 server.listen(3000, handleListen);
